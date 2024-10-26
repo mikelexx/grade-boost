7785d52e055fc75963a09a00cf66448d17df9b5e
mikelexx <michael.muri2020@gmail.com>
2024-10-26 23:14:05 +0300

    fix separate public and private key to expose only the needed keys on client code

--- firebaseConfig.tsx
+++ firebaseConfig.tsx
@@ -1,13 +1,14 @@
 import { initializeApp } from 'firebase/app';
 
+
 const firebaseConfig = {
-  apiKey: process.env.PRIVATE_FIREBASE_API_KEY,
-  authDomain: process.env.PRIVATE_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.PRIVATE_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.PRIVATE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.PRIVATE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.PRIVATE_FIREBASE_APP_ID,
-  measurementId: process.env.PRIVATE_FIREBASE_MEASUREMENT_ID,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET, // No NEXT_PUBLIC_ prefix
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
