947f0ee3cbd9b5d892261cd23670717a781c779c
mikelexx <michael.muri2020@gmail.com>
2024-10-26 21:45:35 +0300

    fix typo in environment variables naming

--- firebaseConfig.tsx
+++ firebaseConfig.tsx
@@ -1,13 +1,13 @@
 import { initializeApp } from 'firebase/app';
 
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_PRIVAE_FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_PRIVAE_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PRIVAE_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_PRIVAE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_PRIVAE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_PRIVAE_FIREBASE_APP_ID,
-  measurementId: process.env.FIREBASE_PRIVAE_FIREBASE_MEASUREMENT_ID,
+  apiKey: process.env.PRIVATE_FIREBASE_API_KEY,
+  authDomain: process.env.PRIVATE_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.PRIVATE_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.PRIVATE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.PRIVATE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.PRIVATE_FIREBASE_APP_ID,
+  measurementId: process.env.PRIVATE_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
